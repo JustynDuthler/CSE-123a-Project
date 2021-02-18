c08f095b0a47ff2cfd75a5363aab7b36d4ff1940
Justyn Duthler <jduthler@ucsc.edu>
2021-02-17 21:05:59 -0800

    inital commit for trash level detection

---
+++ trash level detection/src/levelDetection.c
@@ -0,0 +1,15 @@
+/*********************************
+ * Team 7
+ * 
+ * levelDetection.c
+ *
+ * Description:
+ * This source file contains the code
+ * that implements the level detection
+ * for how much trash is in each bin
+ * and sets the corresponding LEDS
+ * to a color that matches the trash
+ * level. Green = <25% Yellow = <75%
+ * Red = >90%.
+ *
+**********************************/
---
+++ trash level detection/src/levelDetection.h
@@ -0,0 +1,14 @@
+/*********************************
+ * Team 7
+ *
+ * levelDetection.h
+ *
+ * Description:
+ * This header file contains the
+ * functions used within 
+ * levelDetection.c. Each function
+ * is given a description of its 
+ * inputs, outputs, and what it 
+ * does
+ *
+*********************************/
