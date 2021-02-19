ad04ec30c05966d4a5fe78f90d38b99bfafc6750
Justyn Duthler <justynduthler@gmail.com>
2021-02-18 21:25:20 -0800

    sensor code written, need to implement circuit

--- trash level detection/src/levelDetection.c
+++ trash level detection/src/levelDetection.c
@@ -17,24 +17,53 @@
 #include <stdio.h>
 #include <pigpio.h>
 #include <unistd.h>
-
-int main() {
-
-
-    printf("Flickering LED\n");
-    if (gpioInitialise() < 0) {
+#include <sys/time.h>
+int main() 
+{
+    if (initSensors() == 0) {
+        printf("pigpio cannot initialise gpio\nRestart Program\n");
         exit(EXIT_FAILURE);
     }
-    gpioSetMode(26, PI_OUTPUT);
-    for(;;) {
-	time_sleep(1);
-        gpioWrite(26,0);
-	time_sleep(1);
-	gpioWrite(26,1);
+    int recycleDist;
+    for (;;) {
+        recycleDist = calculateDistance(21, 20);
+        printf("Recycle Distance = %d\n", recycleDist);
+        time_sleep(1);
     }
-
-
-
-
+    
     return EXIT_SUCCESS;
+}
+
+int initSensors(void)
+{
+    if (gpioInitialise() < 0) {
+        return 0;
+    }
+    gpioSetMode(21, PI_OUTPUT); // Trigger
+    gpioSetMode(20, PI_INPUT);  // Echo
+    // settle trigger pin
+    gpioWrite(21, 0);
+}
+
+int calculateDistance(int trigPin, int echoPin)
+{
+    // generate trigger pulse 
+    gpioWrite(trigPin, 1);
+    gpioDelay(10);
+    gpioWrite(trigPin, 0);
+    // wait for echo pin to turn high
+    struct timeval startTime, stopTime;
+    while (gpioRead(echoPin) != 1) {
+        gettimeofday(&startTime, NULL); // update start time
+    };
+    // wait for echo pin to turn low
+    while(gpioRead(echoPin) != 0) {
+        gettimeofday(&stopTime, NULL);
+    }
+    // convert time to microseconds;
+    long startTimeM = (startTime.tv_sec + startTime.tv_usec) * 1000000;
+    long stopTimeM = (stopTime.tv_sec + stopTime.tv_usec) * 1000000;
+    long timeDiff = stopTimeM - startTimeM;
+    int distance = timeDiff / 58;
+    return distance;
 }
--- trash level detection/src/levelDetection.h
+++ trash level detection/src/levelDetection.h
@@ -12,3 +12,44 @@
  * does
  *
 *********************************/
+
+/*********************************
+ * int initSensors()
+ * 
+ * INPUTS:
+ *  void
+ * 
+ * OUTPUTS:
+ *  int initSensors: 1 if no issues 
+ *  initilizing sensors, 0 if any 
+ *  issues occured
+ * 
+ * DESCRIPTION:
+ *  This function initilizes the GPIO
+ *  on the pi and sets up the trigger
+ *  and echo pins for the 3 ultrasonic
+ *  sensors
+ * 
+*********************************/
+int initSensors(void);
+
+/*********************************
+ * calculateDistance()
+ * 
+ * INPUTS:
+ *  int trigPin: pin number which the
+ *  trigger pin is connected to 
+ *  int echoPin : pin number which the
+ *  echo pin is connected to
+ * OUTPUTS:
+ *  int distance: distance in cm from
+ *  the ultrasonic sensor
+ * 
+ * DESCRIPTION:
+ *  This function calculates the distance
+ *  the closests object is from the 
+ *  ultrasonic sensor and returns it 
+ *  as an integer in cm.
+ * 
+*********************************/
+int calculateDistance(int trigPin, int echoPin);
