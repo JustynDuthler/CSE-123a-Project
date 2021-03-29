58a6c4a453d5987291b94717fa576ee8fd68c1fd
Justyn Duthler <justynduthler@gmail.com>
2021-03-28 21:20:03 -0700

    added comments to classifcation model training, need to import resnetV2 and train with that

--- image_classification/model.py
+++ image_classification/model.py
@@ -1,3 +1,7 @@
+# Simple Sort
+#
+# Reference: https://www.tensorflow.org/tutorials/images/classification
+
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -8,15 +12,17 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
-
+# path to dataset and printing out number of images
 data_dir = Path('/home/justynduthler/Desktop/CSE-123a-Project/image_classification/compostnet-dataset-resized')
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
 
+# define batch size and image dimensions for training
 batch_size = 32
-img_height = 180
-img_width = 180
+img_height = 200
+img_width = 200
 
+# create training and validation sets from complete dataset
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir,
   validation_split=0.2,
@@ -33,81 +39,23 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
+# define the 7 class names: cardboard, compost, glass, metal, paper, plastic, trash
 class_names = train_ds.class_names
-print(class_names)
 
-plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
-
-plt.show()
-
+# buffer image loading for faster training
 AUTOTUNE = tf.data.AUTOTUNE
-
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
+# process pixel values to fall within 0-1 range
 normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
-
 normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-# Notice the pixels values are now in `[0,1]`.
-print(np.min(first_image), np.max(first_image))
 
+# define number of classes
 num_classes = 7
 
-model = Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
-])
-
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
-model.summary()
-
-epochs=10
-history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
-)
-
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs_range = range(epochs)
-
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
-
+# augment images to yeild more images for training
 data_augmentation = keras.Sequential(
   [
     layers.experimental.preprocessing.RandomFlip("horizontal", 
@@ -119,6 +67,7 @@ data_augmentation = keras.Sequential(
   ]
 )
 
+# define model and layers
 model = Sequential([
   data_augmentation,
   layers.experimental.preprocessing.Rescaling(1./255),
@@ -141,13 +90,15 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-epochs = 15
+# train model for 10 epochs
+epochs = 10
 history = model.fit(
   train_ds,
   validation_data=val_ds,
   epochs=epochs
 )
 
+# print out model results
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
