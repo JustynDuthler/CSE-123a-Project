e76f27b28f72a8bb55445e3c78fa202523561276
Justyn Duthler <justynduthler@gmail.com>
2021-03-29 18:08:24 -0700

    need to fix accuracy

--- image_classification/model.py
+++ image_classification/model.py
@@ -2,101 +2,115 @@
 #
 # Reference: https://www.tensorflow.org/tutorials/images/classification
 
+import PIL
+from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import PIL
 import tensorflow as tf
-from pathlib import Path
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 # path to dataset and printing out number of images
 data_dir = Path('/home/justynduthler/Desktop/CSE-123a-Project/image_classification/compostnet-dataset-resized')
+PATH = os.path.join(os.path.dirname(data_dir), 'compostnet-dataset-resized')
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
 
 # define batch size and image dimensions for training
 batch_size = 32
-img_height = 200
-img_width = 200
+img_size = (160, 160)
 
 # create training and validation sets from complete dataset
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+# create training and validation sets from complete dataset
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir,
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+  validation_split = 0.2,
+  subset = "training",
+  seed = 123,
+  image_size = img_size,
+  batch_size = batch_size)
 
-val_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  data_dir,
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+train_dataset = image_dataset_from_directory(data_dir,
+                                             shuffle=True,
+                                             validation_split = 0.2,
+                                             subset="training",
+                                             seed = 123,
+                                             batch_size=batch_size,
+                                             image_size=img_size)
 
-# define the 7 class names: cardboard, compost, glass, metal, paper, plastic, trash
-class_names = train_ds.class_names
-
+validation_dataset = image_dataset_from_directory(data_dir,
+                                                  shuffle=True,
+                                                  validation_split = 0.2,
+                                                  subset="validation",
+                                                  seed = 123,
+                                                  batch_size=batch_size,
+                                                  image_size=img_size)
+# define the 3 class names: compost, recycling, trash
+class_names = train_dataset.class_names
+print(class_names)
 # buffer image loading for faster training
 AUTOTUNE = tf.data.AUTOTUNE
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+train_dataset = train_dataset.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+validation_dataset = validation_dataset.cache().prefetch(buffer_size=AUTOTUNE)
 
-# process pixel values to fall within 0-1 range
-normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
+# process pixel values to fall within -1, 1 range
+preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
 
 # define number of classes
-num_classes = 7
+num_classes = 3
 
 # augment images to yeild more images for training
-data_augmentation = keras.Sequential(
-  [
-    layers.experimental.preprocessing.RandomFlip("horizontal", 
-                                                 input_shape=(img_height, 
-                                                              img_width,
-                                                              3)),
-    layers.experimental.preprocessing.RandomRotation(0.1),
-    layers.experimental.preprocessing.RandomZoom(0.1),
-  ]
-)
-
-# define model and layers
-model = Sequential([
-  data_augmentation,
-  layers.experimental.preprocessing.Rescaling(1./255),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Dropout(0.2),
-  layers.Flatten(),
-  layers.Dense(128, activation='relu'),
-  layers.Dense(num_classes)
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
+  tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+  tf.keras.layers.experimental.preprocessing.RandomZoom(0.1),
 ])
 
-model.compile(optimizer='adam',
+IMG_SHAPE = img_size + (3,)
+base_model = tf.keras.applications.ResNet50V2(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+image_batch, label_batch = next(iter(train_dataset))
+feature_batch = base_model(image_batch)
+
+# fine tune layers past 100
+base_model.trainable = False
+
+base_model.summary()
+
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+feature_batch_average = global_average_layer(feature_batch)
+
+prediction_layer = tf.keras.layers.Dense(num_classes)
+prediction_batch = prediction_layer(feature_batch_average)
+
+inputs = tf.keras.Input(shape=(160, 160, 3))
+x = data_augmentation(inputs)
+x = preprocess_input(x)
+x = base_model(x, training=False)
+x = global_average_layer(x)
+x = tf.keras.layers.Dropout(0.2)(x)
+outputs = prediction_layer(x)
+model = tf.keras.Model(inputs, outputs)
+
+base_learning_rate = 0.0001
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-
-
 model.summary()
 
+# evaluate model without training
+loss0, accuracy0 = model.evaluate(validation_dataset)
+print("initial loss: {:.2f}".format(loss0))
+print("initial accuracy: {:.2f}".format(accuracy0))
+
 # train model for 10 epochs
-epochs = 10
-history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
-)
+epochs = 15
+history = model.fit(train_dataset,
+                    epochs=epochs,
+                    validation_data=validation_dataset)
+
 
 # print out model results
 acc = history.history['accuracy']
@@ -123,14 +137,42 @@ plt.show()
 
 # basic can test
 can_path = Path('/home/justynduthler/Desktop/CSE-123a-Project/image_classification/can.jpg')
-img = keras.preprocessing.image.load_img(can_path, target_size=(img_height, img_width))
-img_array = keras.preprocessing.image.img_to_array(img)
+img = tf.keras.preprocessing.image.load_img(can_path, target_size=(160, 160))
+img_array = tf.keras.preprocessing.image.img_to_array(img)
 img_array = tf.expand_dims(img_array, 0) # Create a batch
 
 predictions = model.predict(img_array)
 score = tf.nn.softmax(predictions[0])
 
 print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    "This can image most likely belongs to {} with a {:.2f} percent confidence."
     .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
+) 
+
+# basic bag test
+bag_path = Path('/home/justynduthler/Desktop/CSE-123a-Project/image_classification/bag.jpg')
+img = tf.keras.preprocessing.image.load_img(bag_path, target_size=(160, 160))
+img_array = tf.keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This bag image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+) 
+
+# basic banana peel test
+ban_path = Path('/home/justynduthler/Desktop/CSE-123a-Project/image_classification/ban.jpg')
+img = tf.keras.preprocessing.image.load_img(ban_path, target_size=(160, 160))
+img_array = tf.keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This banana image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+) 
