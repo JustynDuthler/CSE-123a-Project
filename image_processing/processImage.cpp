40dd0a426aa86ca4c941025b5e61e2fc7c0d3fdf
Kevin Ryan <Kevin@macbook-pro.lan>
2021-04-02 12:18:36 -0700

    image capture and display

---
+++ image_processing/processImage.cpp
@@ -0,0 +1,38 @@
+#include <chrono>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <sstream>
+#include <stdio.h>
+
+// Read an image from the camera
+static cv::Mat GetImageFromCamera(cv::VideoCapture& camera)
+{
+    cv::Mat frame;
+    camera >> frame;
+    return frame;
+}
+
+// Read an image from a file
+static cv::Mat GetImageFromFile(const std::string& filename)
+{
+    return cv::imread(filename);
+}
+
+int main(int argc, char** argv)
+{
+
+
+	while ((cv::waitKey(1) & 0xFF) != 27)
+    {
+        // Get an image from the camera. (Alternatively, call GetImageFromFile to read from file)
+        //cv::Mat image = GetImageFromCamera(camera);
+        cv::Mat image = GetImageFromFile(argv[1]);
+
+
+        // Display the image
+        cv::imshow("SimpleSort", image);
+    }
+
+
+	return 0;
+}
