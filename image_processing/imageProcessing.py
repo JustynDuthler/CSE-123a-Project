86f6cd709a5a57cbd7a2042a017f3d15727aab55
Kevin Ryan <Kevin@macbook-pro.lan>
2021-04-05 17:38:00 -0700

    try without loop

--- image_processing/imageProcessing.py
+++ image_processing/imageProcessing.py
@@ -33,33 +33,33 @@ def main():
     img_height = 224
     img_width = 224
 
-    while (cv2.waitKey(1) & 0xFF) == 0xFF:
-        # Get an image from the camera.
-        image = get_image_from_camera(camera)
+    #while (cv2.waitKey(1) & 0xFF) == 0xFF:
+    # Get an image from the camera.
+    image = get_image_from_camera(camera)
 
-        resized_img = cv2.resize(image, (img_height, img_width))
+    resized_img = cv2.resize(image, (img_height, img_width))
 
-        # Display the image
-        cv2.imshow("SimpleSort", image)
+    # Display the image
+    cv2.imshow("SimpleSort", image)
 
-        X = np.array(resized_img)
-        X = X / 255
+    X = np.array(resized_img)
+    X = X / 255
 
-        # increases dimension by 1
-        X = tf.expand_dims(X, 0) # Create a batch
-        #print(X.shape)
+    # increases dimension by 1
+    X = tf.expand_dims(X, 0) # Create a batch
+    #print(X.shape)
 
-        #model = tf.keras.models.load_model('saved-model/model')
+    model = tf.keras.models.load_model('saved-model/model')
 
-        #prediction = model.predict(X)
-        #score = tf.nn.softmax(prediction[0])
+    prediction = model.predict(X)
+    score = tf.nn.softmax(prediction[0])
 
-        #class_names = ["cardboard", "compost", "glass", "metal", "paper", "plastic", "trash"]
+    class_names = ["cardboard", "compost", "glass", "metal", "paper", "plastic", "trash"]
 
-       # print(
-       #     "This image most likely belongs to {} with a {:.2f} percent confidence."
-       #     .format(class_names[np.argmax(score)], 100 * np.max(score))
-       # )
+     print(
+            "This image most likely belongs to {} with a {:.2f} percent confidence."
+            .format(class_names[np.argmax(score)], 100 * np.max(score))
+        )
    
 
 if __name__ == "__main__":
