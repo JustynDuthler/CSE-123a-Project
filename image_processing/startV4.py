fd3d7f82e1fcec04ab85a25c80c0ddf44a2933bb
Kevin Ryan <Kevin@macbook-pro.lan>
2021-05-01 12:03:00 -0700

    startv4 uses tflite model and adjusts motion detection sensitivity

--- image_processing/startV4.py
+++ image_processing/startV4.py
@@ -64,7 +64,7 @@ def main():
             difference = cv2.subtract(image1, image2)
             b, g, r = cv2.split(difference)
 
-            if cv2.countNonZero(b) < 60 and cv2.countNonZero(g) < 60 and cv2.countNonZero(r) < 60:
+            if cv2.countNonZero(b) < 50 and cv2.countNonZero(g) < 50 and cv2.countNonZero(r) < 50:
                 print("motion not detected")
                 motion = 0
             else :
@@ -73,7 +73,7 @@ def main():
 
         
         	
-            time.sleep(1)
+            time.sleep(0.5)
 
             image2 = get_image_from_camera(camera)
             image2 = cv2.resize(image2, (10, 10))
@@ -82,51 +82,56 @@ def main():
         # Get an image from the camera.
         image = get_image_from_camera(camera)
 
+        # taken from tfliteTest
+        interpreter = tf.lite.Interpreter(model_path="model.tflite")
+
+        input_details = interpreter.get_input_details()
+        output_details = interpreter.get_output_details()
+
+        interpreter.allocate_tensors()
+
         resized_img = cv2.resize(image, (img_height, img_width))
 
-        # Display the image
-        #cv2.imshow("SimpleSort", image)
+        # display the image
+        cv2.imshow("SimpleSort", image)
 
-        X = np.array(resized_img)
-        X = X / 255
+        resized_img = np.asarray(resized_img, dtype='float32')
+        resized_img /= 255.0
+        tf.shape(tf.squeeze(resized_img))
 
-        # increases dimension by 1
-        X = tf.expand_dims(X, 0) # Create a batch
+        interpreter.set_tensor(input_details[0]['index'], [resized_img])
+
+        interpreter.invoke()
+
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+
+        print("The output is {}".format(output_data))
+
+        max = np.max(output_data)
+        print(max)
+
+        num = np.argmax(output_data)
     
-
-        model = tf.keras.models.load_model('saved-model/model')
-
-        prediction = model.predict(X)
-        score = tf.nn.softmax(prediction[0])
-
-        #class_names = ["cardboard", "compost", "glass", "metal", "paper", "plastic", "trash"]
-
-        #print(
-        #    "This image most likely belongs to {} with a {:.2f} percent confidence."
-        #    .format(class_names[np.argmax(score)], 100 * np.max(score))
-        #)
-   
-        cat = np.argmax(score)
-
-        if cat == 0 or cat == 2 or cat == 3 or cat == 4 or cat == 5:
+        if num == 0 or num == 2 or num == 3 or num == 4 or num == 5 or num == 5:
             print("recycle, gpio 4")
             pin4.on()
             pin5.off()
             time.sleep(2)
 
-        elif cat == 1:
-            print("compost, gpio 5")
+        elif num == 1:
+            print("compost, gpio5")
             pin4.off()
             pin5.on()
             time.sleep(2)
-
-        elif cat == 6:
+        elif num == 6:
             print("trash, gpio 4 & gpio 5")
             pin4.on()
             pin5.on()
             time.sleep(2)
-        
     
+    #wait for sorting to finish
+    print("waiting for sorting to finish")    
+    time.sleep(2)
    
 
 if __name__ == "__main__":
